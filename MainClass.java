3af57e2a71842765e2551f589aa1d95175bfac9f
Pawan Kumar <pk.soft29@gmail.com>
2020-10-29 16:24:05 +0530

    Added : Complete AddressBook project

---
+++ MainClass.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        HashMap<String, AddressBook> addressBookList = new HashMap<String, AddressBook>();
+
+        AddressBook familyAddressBook = new AddressBook();
+        AddressBook friendsAddressBook = new AddressBook();
+        addressBookList.put("Family", familyAddressBook);
+        addressBookList.put("Friends", friendsAddressBook);
+
+        while (true) {
+            System.out.println("1: for family \n" +
+                    "2: for friend \n" +
+                    "3: for terminate the program");
+            int selectedOption = scanner.nextInt();
+            if (selectedOption == 1) {
+                familyAddressBook.getUserChoice();
+            }
+            else if (selectedOption == 2) {
+                friendsAddressBook.getUserChoice();
+            }
+            else if(selectedOption == 3) {
+                break;
+            }
+        }
+    }
+}
